8a731692fa593545f74d326c49337bb05ff70530
stevenbaum <stevenmichaelbaum@gmail.com>
2013-08-08 15:18:51 -0700

    Changed goalconfigs to hold Blocks

--- Solver.java
+++ Solver.java
@@ -10,18 +10,20 @@ public class Solver {
     
     /** ArrayList of goal configurations.
      * Each element is a string representing a block in the goal board. */
-    public static ArrayList<String> goalconfigs = new ArrayList<String>();
+    public static ArrayList<Block> goalconfigs = new ArrayList<Block>();
     
     /** HashSet containing all seen board configurations.
      * Eliminates redundancy in creating board objects */
     public static HashSet<Board> boardset = new HashSet<Board>();
     
+    /** Current path being navigated in the graph. Draws from the fringe
+     * (priority queue) to grow or shrink it. */
+    public static Stack<Board> currentpath = new Stack<Board>();
+    
     /** Priority Queue containing a prioritized set of potential boards.
      * Based on heuristic evaluation boards are ranked as more or less desirable,
      * and consequently selected to eventually find the goal configuration. */
     public static PriorityQueue<Board> prioritylist = new PriorityQueue<Board>();
-    
-    
     
     /** Create a Solver object's fields based on program inputs. */
     public Solver (String[] args) {
@@ -36,6 +38,7 @@ public class Solver {
         }
     }
     
+    
     /* 
      * Return true if the configuration of Board b has blocks of the
      * right size placed in the right locations as indicated by the
@@ -43,16 +46,16 @@ public class Solver {
      */
     public static boolean compareToGoal(Board b) {
         // myBlocks is unimplemented and represents the ArrayList of
-    	// blocks stored in each Board object
-    	boolean[] results = new boolean[goalconfigs.size()];
-    	boolean result = false;
-    	for (int i = 0; i < goalconfigs.size(); i++) {
-    		results[i] = boardContainsGoalBlock(b, goalconfigs.get(i));
-    	}
-    	for (int i = 1; i < results.length; i++) {
-    		result = results[i] && results[i-1];
-    	}
-    	return result;
+        // blocks stored in each Board object
+        boolean[] results = new boolean[goalconfigs.size()];
+        boolean result = false;
+        for (int i = 0; i < goalconfigs.size(); i++) {
+            results[i] = boardContainsGoalBlock(b, goalconfigs.get(i));
+        }
+        for (int i = 1; i < results.length; i++) {
+            result = results[i] && results[i-1];
+        }
+        return result;
     }
     
     /*
@@ -60,18 +63,22 @@ public class Solver {
      * contains a block of the right size and location specified by
      * goalblock
      */
-    private static boolean boardContainsGoalBlock(Board b, String goalblock) {
-		for (int j = 0; j < b.blocklist.size(); j++) {
-			if (b.blocklist.get(j).toString().equals(goalblock)) {
-				return true;
-			}
-		}
-    	return false;
+    private static boolean boardContainsGoalBlock(Board b, Block goalblock) {
+        for (int j = 0; j < b.blocklist.size(); j++) {
+            if (b.blocklist.get(j).equals(goalblock)) {
+                return true;
+            }
+        }
+        return false;
     }
     
     
     public static void main(String[] args) {
         
+        if (args.length == 0 || args.length > 3 || (args.length == 3 && !args[0].contains("-o"))) {
+            System.err.println("Must provide at least 2 or 3 files");
+            System.exit(1);
+        }
         Solver solve = new Solver(args);
         // Using input sources, create initial board and populate
         // goalconfig with all of the lines needed to solve the puzzle
@@ -95,11 +102,16 @@ public class Solver {
         // goalconfigs ArrayList
         s = solve.goalinput.readLine();
         while (s != null) {
-            goalconfigs.add(s);
+            // Create a Block out of goal configuration, store in ArrayList
+            String[] solution = s.split(" ");
+            int row1 = Integer.parseInt(solution[0]);
+            int col1 = Integer.parseInt(solution[1]);
+            int row2 = Integer.parseInt(solution[2]);
+            int col2 = Integer.parseInt(solution[3]);
+            Block b = new Block(row1, col1, row2, col2);
+            goalconfigs.add(b);
             s = solve.goalinput.readLine();
         }
-        
-        
 
     }
 
