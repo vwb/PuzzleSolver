7be2693b68de75d6c2d93745d37153a674d37b31
stevenbaum <stevenmichaelbaum@gmail.com>
2013-08-10 22:22:38 -0700

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,29 +1,45 @@
 import java.util.*;
 
+/** Sliding block puzzle solver that utilizes a greedy search algorithm
+ * and value-based heuristics to find the (possibly nonexistant) solution
+ * configuration.  */
+
 public class Solver {
 
     /** InputSource object used to parse goal configuration file. */
-    public InputSource goalinput;
+    private InputSource goalinput;
     
     /** InputSource object used to parse initial board configuration. */
-    public InputSource boardinput;
+    private InputSource boardinput;
     
     /** ArrayList of goal configurations.
      * Each element is a string representing a block in the goal board. */
-    public static ArrayList<Block> goalconfigs = new ArrayList<Block>();
+    public ArrayList<Block> goalconfigs;
     
-    /** HashSet containing all seen board configurations.
-     * Eliminates redundancy in creating board objects */
-    public static HashSet<Board> boardset = new HashSet<Board>();
+    /** HashMap containing all seen board configurations.
+     * Eliminates redundancy in creating board objects
+     * When a given board is checking its potential moves, it checks
+     * this hashSet first to see if that potential move already exists.
+     * This avoids re-evaluating heuristics and will
+     * Instead grab the board in this set. */
+    private HashMap<Board, Board> seenboardmap;
+    
+    /** HashSet of board configurations actually selected as a part of the 
+     * greedy search algorithm */
+    private HashSet<Board> chosenboardset;
     
     /** Current path being navigated in the graph. Draws from the fringe
      * (priority queue) to grow or shrink it. */
-    public static Stack<Board> currentpath = new Stack<Board>();
+    private LinkedList<String> currentpath;
+    
+    public LinkedList<String> currentpath() {
+        return currentpath;
+    }
     
     /** Priority Queue containing a prioritized set of potential boards.
      * Based on heuristic evaluation boards are ranked as more or less desirable,
      * and consequently selected to eventually find the goal configuration. */
-    public static PriorityQueue<Board> priorityqueue = new PriorityQueue<Board>();
+    public static PriorityQueue<Board> priorityqueue;
     
     /** Create a Solver object's fields based on program inputs. */
     public Solver (String[] args) {
@@ -36,6 +52,12 @@ public class Solver {
             boardinput = new InputSource(args[1]);
             goalinput = new InputSource(args[2]);
         }
+        
+        goalconfigs = new ArrayList<Block>();
+        seenboardmap = new HashMap<Board, Board>();
+        chosenboardset = new HashSet<Board>();
+        currentpath = new LinkedList<String>();
+        priorityqueue = new PriorityQueue<Board>();
     }
     
     
@@ -44,13 +66,16 @@ public class Solver {
      * right size placed in the right locations as indicated by the
      * goal configuration
      */
-    public static boolean compareToGoal(Board b) {
+    public boolean compareToGoal(Board b) {
         // myBlocks is unimplemented and represents the ArrayList of
         // blocks stored in each Board object
         boolean[] results = new boolean[goalconfigs.size()];
         boolean result = false;
         for (int i = 0; i < goalconfigs.size(); i++) {
             results[i] = boardContainsGoalBlock(b, goalconfigs.get(i));
+        }
+        if (results.length == 1) {
+            return results[0];
         }
         for (int i = 1; i < results.length; i++) {
             result = results[i] && results[i-1];
@@ -87,32 +112,62 @@ public class Solver {
             Block right = block.moveright();
             
             // Check downward movement
-            if (block.checkdown(board) && !boardset.contains(new Board(board, block, down))) {
+            if (block.checkdown(board)) {
                 Board downboard = new Board(board, block, down);
-                downboard.setHeuristic();
-                boardset.add(downboard);
-                priorityqueue.add(downboard);
+                // If this board was already chosen, ignore it
+                if (!chosenboardset.contains(downboard)) {
+                    // If this board is a previously seen, but not chosen
+                    // Configuration, return the already-evaluated board from
+                    // the HashMap to spare unnecessary heuristic tests
+                    if (seenboardmap.containsKey(downboard)) {
+                        priorityqueue.add(seenboardmap.get(downboard));
+                    } else {
+                        // New board is never before seen, evaluate its heuristic
+                        // and mark it as seen; also place in priority queue
+                        downboard.setHeuristic(this);
+                        seenboardmap.put(downboard, downboard);
+                        priorityqueue.add(downboard);
+                    }
+                }
             }
             // Check upward movement
-            if (block.checkup(board) && !boardset.contains(new Board(board, block, up))) {
+            if (block.checkup(board)) {
                 Board upboard = new Board(board, block, up);
-                upboard.setHeuristic();
-                boardset.add(upboard);
-                priorityqueue.add(upboard);
+                if (!chosenboardset.contains(upboard)) {
+                    if (seenboardmap.containsKey(upboard)) {
+                        priorityqueue.add(seenboardmap.get(upboard));
+                    } else {
+                        upboard.setHeuristic(this);
+                        seenboardmap.put(upboard, upboard);
+                        priorityqueue.add(upboard);
+                    }
+                }
+
             }
             // Check leftward movement
-            if (block.checkleft(board) && !boardset.contains(new Board(board, block, left))) {
+            if (block.checkleft(board)) {
                 Board leftboard = new Board(board, block, left);
-                leftboard.setHeuristic();
-                boardset.add(leftboard);
-                priorityqueue.add(leftboard);
+                if (!chosenboardset.contains(leftboard)) {
+                    if (seenboardmap.containsKey(leftboard)) {
+                        priorityqueue.add(seenboardmap.get(leftboard));
+                    } else {
+                        leftboard.setHeuristic(this);
+                        seenboardmap.put(leftboard, leftboard);
+                        priorityqueue.add(leftboard);
+                    }
+                }
             }
             // Check rightward movement
-            if (block.checkright(board) && !boardset.contains(new Board(board, block, right))) {
+            if (block.checkright(board)) {
                 Board rightboard = new Board(board, block, right);
-                rightboard.setHeuristic();
-                boardset.add(rightboard);
-                priorityqueue.add(rightboard);
+                if (!chosenboardset.contains(rightboard)) {
+                    if (seenboardmap.containsKey(rightboard)) {
+                        priorityqueue.add(seenboardmap.get(rightboard));
+                    } else {
+                        rightboard.setHeuristic(this);
+                        seenboardmap.put(rightboard, rightboard);
+                    }
+                }
             }
         }
     }
@@ -132,15 +187,19 @@ public class Solver {
         String[] dimensions = s.split(" ");
         int row = Integer.parseInt(dimensions[0]);
         int column = Integer.parseInt(dimensions[1]);
-        Board board = new Board(row, column);
+        Board initboard = new Board(row, column);
         
         // Continue parsing the initial configuration file, populating the board
         // with each line parsed
         s = solve.boardinput.readLine();
         while (s != null) {
-            board.populateBoard(s);
+            initboard.populateBoard(s);
             s = solve.boardinput.readLine();
         }
+        // Add initial board to the HashSet
+        initboard.setHeuristic(solve);
+        solve.seenboardmap.put(initboard, initboard);
+        
         
         // Parse the solution file, adding each line to the
         // goalconfigs ArrayList
@@ -153,10 +212,45 @@ public class Solver {
             int row2 = Integer.parseInt(solution[2]);
             int col2 = Integer.parseInt(solution[3]);
             Block b = new Block(row1, col1, row2, col2);
-            goalconfigs.add(b);
+            solve.goalconfigs.add(b);
             s = solve.goalinput.readLine();
         }
 
+        // Initial board is already the solution? Guess you're done, congrats
+        if (solve.compareToGoal(initboard)) {
+            System.exit(1);
+        }
+        // Populate priority queue with initial move choices, select best one
+        solve.generatemoves(initboard);        
+        Board current = priorityqueue.poll();
+        solve.currentpath.add(current.getdefine());
+        
+        
+        // While there are still legal moves to be made
+        while (!solve.compareToGoal(current)) {
+            // Clear queue for new set of possible moves
+            priorityqueue.clear();
+            // Generate board's possible moves, select best one
+            solve.generatemoves(current);
+            // If no moves were generated, all of this board's moves
+            // Were previously seen; "dead end"; puzzle unsolvable
+            if (priorityqueue.isEmpty()) {
+                System.exit(1);
+            }
+            current = priorityqueue.poll();
+            solve.currentpath.addFirst((current.getdefine()));
+            
+        }
+        
+        // Exited while loop, solution was found, print out
+        // Path taken
+        while (!solve.currentpath.isEmpty()) {
+            System.out.println(solve.currentpath.getFirst());
+            solve.currentpath.removeFirst();
+        }
+
+            
+        
     }
 
 }
